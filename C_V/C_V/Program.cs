143b6081f13c5342d259205561cf677917154757
Anastasia <90788725+nstnkp@users.noreply.github.com>
2021-09-20 18:13:25 +0300

    Visener

---
+++ C_V/C_V/Program.cs
@@ -0,0 +1,24 @@
+﻿using C_V;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VigenerCipher cipher = new VigenerCipher();
+
+
+        Console.Write("Enter text: ");
+        string input = Console.ReadLine().ToUpper();
+        Console.Write("\nEnter key: ");
+        string key = Console.ReadLine().ToUpper();
+
+        Console.WriteLine("Зашифрованный текст");
+        string encryptedText = cipher.EncryptText(input, key);
+        Console.WriteLine(encryptedText);
+
+        Console.WriteLine("Расшифрованный текст");
+        string decryptedText = cipher.DecryptedText(encryptedText);
+        Console.WriteLine(decryptedText);
+    }
+}
---
+++ C_V/C_V/VigenerCipher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_V
+{
+    class VigenerCipher
+    {
+        const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        readonly string letters;
+
+        string tempKey = "";
+
+        
+
+        public VigenerCipher(string alphUser = null)
+        {
+            letters = string.IsNullOrEmpty(alphUser) ? alphabet : alphUser;
+        }
+
+        public string RepeatKey(string key, int n)
+        {
+            string tempKey = key;
+
+            while (tempKey.Length < n)
+            {
+                tempKey += tempKey;
+            }
+
+            return tempKey.Substring(0, n);
+        }
+
+        public string EncryptText(string text, string key)
+        {
+            tempKey = RepeatKey(key, text.Length);
+
+            string encryptedText = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                encryptedText += alphabet[(alphabet.Length + text[i] + tempKey[i]) % alphabet.Length];
+            }
+
+            return encryptedText;
+        }
+
+        public string DecryptedText(string encrText)
+        {
+            string decrText = "";
+
+            for (int i = 0; i < encrText.Length; i++)
+            {
+                decrText += alphabet[(alphabet.Length + encrText[i] - tempKey[i]) % alphabet.Length]; 
+            }
+
+            return decrText;
+        }
+
+
+    }
+}
